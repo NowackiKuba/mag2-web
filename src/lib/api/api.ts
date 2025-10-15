006a8d39c992dacedc14e0fde6ea534d7a9580f4
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2025-10-15 12:17:46 +0200

    prod

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${import.meta.env.BACKEND_PROD_URL}/api/v1`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
